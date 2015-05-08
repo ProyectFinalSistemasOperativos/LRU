2ed3fbc494935ad741efebe7b4485e6ec5271442
balberry <balbinasantana@gmail.com>
2015-05-08 15:34:08 -0500

    probando fstreams

--- LRU/LRU/main.cpp
+++ LRU/LRU/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "Pagina.h"
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -19,11 +20,12 @@ int main(int argc, const char * argv[]) {
     entrada.open ("input.txt");
     string linea;
     entrada >>linea;//P 2048 1
+                    //cout <<entrada <<endl;
     entrada.close();
     
     ofstream salida;
     salida.open("output.txt");
-    salida <<"escribir o variables al text. \n";
+    salida <<"escribir o variables al text.";
     salida.close();
     
     return 0;
